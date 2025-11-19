b22a070e1e2bf30bcf1845d23609d588842d6546
alejandropf3 <alejandropefa31@gmail.com>
2025-11-19 16:01:25 -0500

    Realizamos el ejercicio a de la guia

---
+++ Ejercicio_a.js
@@ -0,0 +1,32 @@
+const frutas = [ //Inicializamos el arreglo
+    "Manzana", "Pera", "Banano"
+];
+
+// • Realice las siguientes operaciones y escriba el resultado final del arreglo:
+// • Agregue "Mango" al final.
+// • Elimine el último elemento.
+// • Agregue "Uva" al inicio.
+// • Elimine el primer elemento.
+// • Escriba cuántos elementos tiene el arreglo actualmente usando length.
+
+frutas.unshift("Uva"); // añadimos el dato en la primera posicion
+console.log(frutas); //Mostramos el arreglo
+console.log(frutas.length) //Mostramos el numero de elementos en el arreglo
+
+frutas.pop();
+console.log(frutas);
+console.log(frutas.length)
+
+frutas.push("Mango");
+console.log(frutas);
+console.log(frutas.length)
+
+frutas.shift();
+console.log(frutas);
+console.log(frutas.length)
+
+// sistema añadira el dato "Uva" al principio y luego lo eliminara
+//Luego agregara el elemento mango en la ultima posicion y luego lo eliminara
+
+
+
